906b60fd10a28536e5e795a562957efbb0bf9e89
bgurzick <bgurzick@gmail.com>
2024-07-30 00:14:47 -0700

    commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,8 +5,8 @@ function Navigation() {
   return (
     <nav className="menu">
       <ul>
-        <li><Link to="/my-profile">My Profile</Link></li>
-        <li><Link to="/daily-schedule">Daily Schedule</Link></li>
+        <li><Link to="/my-profile">my profile</Link></li>
+        <li><Link to="/daily-schedule">daily schedule</Link></li>
         <li><Link to="/speech-practice">Speech Practice</Link></li>
         <li><Link to="/stroke-centers">Stroke Centers</Link></li>
       </ul>
